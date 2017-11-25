4bbbec8b197b39f84ea64e6b29b6c8a7272b0974
Marek Piskorek <marek.piskorek@gmail.com>
2017-11-25 17:32:38 +0100

    Initial commit with rewritten code

---
+++ justsoin.py
@@ -0,0 +1,24 @@
+from pprint import pprint
+import requests
+
+
+def get_all_offers_from_justjoinit():
+    """ API call doesn't seem to respect any filters, we may need to filter
+    offers manually in JSON / Mongo. """
+    url = 'https://justjoin.it/api/offers'
+
+    headers = {
+        'content-type': 'application/json, text/plain',
+        'User-Agent': (
+            'Mozilla/5.0 (X11; Linux x86_64; rv:57.0) '
+            'Gecko/20100101 Firefox/57.0'
+        ),
+        'Host': 'justjoin.it',
+        'Referer': 'justjoin.it',
+    }
+    response = requests.get(url, headers=headers)
+    pprint(response.json())
+
+
+if __name__ == '__main__':
+    get_all_offers_from_justjoinit()
---
+++ nofluff.py
@@ -0,0 +1,24 @@
+from pprint import pprint
+import requests
+
+
+def get_data_from_nofluff():
+    url = (
+        'https://nofluffjobs.com/api/search/posting'
+        '?criteria=python+city%3Dwarszawa+category%3Dbackend'
+    )
+    headers = {
+        'content-type': 'application/json, text/plain',
+        'User-Agent': (
+            'Mozilla/5.0 (X11; Linux x86_64; rv:57.0) '
+            'Gecko/20100101 Firefox/57.0'
+        ),
+        'Host': 'noflufjobs.com',
+        'Referer': 'https://nofluffjobs.com',
+    }
+    response = requests.get(url, headers=headers)
+    pprint(response.json())
+
+
+if __name__ == '__main__':
+    get_data_from_nofluff()
